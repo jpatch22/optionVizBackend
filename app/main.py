a1957fc430889684ad1a786315a32fde778b3aeb
Jack Patchell <jackpatchell222@gmail.com>
2024-07-15 07:34:13 -0700

    allow options request

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.endpoints import calculations
 from app.api.v1.endpoints import binomial_asset
 
 app = FastAPI()
+
+# Set up CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to limit origins allowed to access your API
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods, or specify allowed methods like ["GET", "POST"]
+    allow_headers=["*"],  # Allow all headers, or specify allowed headers
+)
 
 app.include_router(calculations.router, prefix="/calculations", tags=["calculations"])
 app.include_router(binomial_asset.router, prefix="/binomial_asset", tags=["binomial_asset"])
